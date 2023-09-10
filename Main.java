4e7ddeddde1affada6a3d6244860e6d61e6bcf4b
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-09-10 17:31:13 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    Salesman mySalesman = new Salesman();
+    Salesman[] allSalesmen = new Salesman[10];
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
---
+++ Salesman.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Salesman
+{   
+    String name; 
+    float sales;
+    
+    Salesman[] temp = new Salesman[5];
+    float[] extraTemp = new float[1];
+    int startingTempNum = 0;
+    
+    
+    Salesman[] getTopFive(Salesman[] allSales)
+    {
+        for (int i = 0; i < allSales.length-1; i++)
+        {
+            float maxNumber = 0; 
+            
+            if (allSales[i].sales > maxNumber)
+            {
+                if (startingTempNum == 5)
+                {
+                    return temp;
+                }
+                else if (startingTempNum >= 1)
+                {
+                    if (allSales[i].sales > allSales[i-1].sales)
+                    {
+                        extraTemp[0] = allSales[i-1].sales;
+                        allSales[i-1].sales = allSales[i].sales;
+                        allSales[i].sales = extraTemp[0];
+                        startingTempNum++;
+                    }
+                }
+                else if (startingTempNum == 0)
+                {
+                    temp[startingTempNum] = allSales[i];
+                    startingTempNum++;
+                }
+            }
+        }
+        
+        return temp;
+    }
+}
+
